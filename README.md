27b0d76f62f0c098ff4cfe2ae24d0d041576914a
Craig <indigomx9@gmail.com>
2022-07-07 20:43:53 -0400

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Redux-Toolkit CRUD
+
+Initial setup of the project.  Created with React Router, JSON Server, React, and Redux-Toolkit.
+
+
+
---
+++ src/app/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, officiis aliquam laudantium obcaecati sed exercitationem distinctio aperiam voluptates reprehenderit nostrum amet. Blanditiis possimus facere doloremque, harum nobis aspernatur eum praesentium.</p>
+        </React.Fragment>
+    );
+};
+
+
